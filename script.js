72d6a3ef18f2c93db2bd3ac9bdde9476ff397e21
angelorapana <angelorapana@icloud.com>
2026-01-23 09:42:47 +0100

    REVISIONE: creao variabile con elemento dom

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Bonus
 // Rendere l’esercizio responsive, mandando a capo le card
 // Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! 😀)
-
+/*
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -48,16 +48,16 @@ const listaHtml = document.getElementById("elementi"); //Prendo l'id dove occorr
 
 //Creo la funzione che recupera i dati 
 const creaNuovoMembro = (nuovoMembro) => {
-        const  {name, role, email} = nuovoMembro
-        const aggiungiMembro = `
-                    <div class="col-3">
-                    <div class="card" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">${name}</h5>
-                    <p class="card-text">${email}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>`
+        const  {name, role, email, img} = nuovoMembro
+        const aggiungiMembro = ` <div class="card d-flex flex-row">
+                                 <img src="${img}" alt="Immagine profilo" style="width: 10rem; border-top-left-radius: 1rem; border-bottom-left-radius: 1rem;">
+                                 <div class="p-3">
+                                 <h5 class="fw-bold">Nome: ${name}</h5>
+                                 <p>Ruolo: ${role}</p>
+                                 <p>Email: ${email}</p>
+                                 </div>
+                                 </div>
+                                 </div>`
                     return aggiungiMembro;
     } 
 
@@ -68,9 +68,54 @@ const teamMemberHtml = creaNuovoMembro(teamMember)
 
 const newMember = document.createElement("li")  //creo una variabile contenente l'elemento da inserire nel dom
 newMember.innerHTML = teamMemberHtml;           //utilizzo la variabile e aggiungo al dom (con innerhtml) attraverso la funzione
-listaHtml.append(nuovePersone); }               //lo stampo nel dom con .append
+listaHtml.append(newMember); }               //lo stampo nel dom con .append
+
+*/
 
 
-   
+
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+
+const teamContainer = document.getElementById("team-container")
+
+c
 
 
